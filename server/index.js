9508b4635cdb365d574c9b84090dfc6f37786097
Harris <Harris.mo@protonmail.ch>
2020-12-07 17:50:25 +0530

    MongoDB Connection

--- server/index.js
+++ server/index.js
@@ -5,6 +5,23 @@ const bodyParser = require('body-parser');
 
 
 
+
+
+const mongoose = require("mongoose")
+const uri = "mongodb://127.0.0.1:27017/locsearch";
+
+mongoose.connect(uri, { useUnifiedTopology: true, useNewUrlParser: true });
+
+const connection = mongoose.connection;
+
+connection.once("open", function () {
+  console.log("MongoDB database connection established successfully");
+});
+
+
+
+
+
 const PORT = 5000;
 
 app.use(bodyParser.urlencoded({ extended: false }));
